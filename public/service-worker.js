3a7189412b86cd06f572c4c8a293699c9d7dab21
daejo <daejo@icloud.com>
2020-12-05 19:06:27 -0800

    adds list of files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,13 @@
+// LIST OF FILES TO BE CACHED //
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    // "./dist/app.bundle.js",
+    // "./dist/events.bundle.js",
+    // "./dist/tickets.bundle.js",
+    // "./dist/schedule.bundle.js"
+];
+
 self.addEventListener('install', function (e) {
 
 })
