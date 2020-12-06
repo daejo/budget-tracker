111c74379065fb1c33130e2ff0f4d7a6b8a6b11d
daejo <daejo@icloud.com>
2020-12-05 23:53:12 -0800

    adds in more files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,8 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 // LIST OF FILES TO BE CACHED //
 const FILES_TO_CACHE = [
     "./index.html",
-    "./css/styles.css"
+    "./css/styles.css",
+    "./js/index.js"
 ];
 
 self.addEventListener('install', function (e) {
