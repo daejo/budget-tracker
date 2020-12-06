a49592b76cd8950d9dbb02c187ab2cc33147f643
daejo <daejo@icloud.com>
2020-12-06 01:06:38 -0800

    updates idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,73 +1,60 @@
 let db;
-const request = indexedDB.open('transaction', 1);
+
+const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-// upon a successful 
 request.onsuccess = function(event) {
     db = event.target.result;
-  
+
     if (navigator.onLine) {
-      uploadTransaction();
+        addRecord();
     }
-  };
-  
-  request.onerror = function(event) {
+};
+
+request.onerror = function(event) {
     console.log(event.target.errorCode);
-  };
+};
 
-  
 function saveRecord(record) {
-   
     const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerStore = transaction.objectStore('new_transaction');
 
-    const transactionObjectStore = transaction.objectStore('new_transaction');
-  
-    transactionObjectStore.add(record);
+    trackerStore.add(record);
 }
 
-function uploadTransaction() {
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
+function addRecord() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const trackerStore = transaction.objectStore('new_transaction');
+    const getAll = trackerStore.getAll();  
 
-
-  const transactionObjectStore = transaction.objectStore('new_transaction');
-
-  const getAll = transactionObjectStore.getAll();
-
-  
-  getAll.onsuccess = function() {
-
-    if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
-        method: 'POST',
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const trackerStore = transaction.objectStore('new_transaction');
+                    trackerStore.clear();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
-      })
-        .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-       
-          const transaction = db.transaction(['new_transaction'], 'readwrite');
-        
-          const transactionObjectStore = transaction.objectStore('new_transaction');
-       
-          transactionObjectStore.clear();
-
-          alert('All saved transaction has been submitted!');
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  };
+    };
 }
 
-window.addEventListener('online', uploadTransaction);
+window.addEventListener('online', addRecord);
