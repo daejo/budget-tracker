c03e79a2e2e4d7c2778e22bee7952c5131850b5a
daejo <daejo@icloud.com>
2020-12-05 20:05:15 -0800

    fixes cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
-const APP_PREFIX = 'FoodFest-';     
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 // LIST OF FILES TO BE CACHED //
 const FILES_TO_CACHE = [
     "./index.html",
-    "./css/style.css",
+    "./css/styles.css",
     // "./dist/app.bundle.js",
     // "./dist/events.bundle.js",
     // "./dist/tickets.bundle.js",
@@ -20,3 +20,21 @@ self.addEventListener('install', function (e) {
         })
     )
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  });
