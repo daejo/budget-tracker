3692be6a59909a8163a9d610f09b537d95424315
daejo <daejo@icloud.com>
2020-12-06 00:18:16 -0800

    updates service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,7 @@
-const APP_PREFIX = 'BudgetTracker-';     
+const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = 'data-cache-' + VERSION;
-
-// LIST OF FILES TO BE CACHED //
 const FILES_TO_CACHE = [
     "./index.html",
     "./js/idb.js",
@@ -18,7 +16,6 @@ const FILES_TO_CACHE = [
     "./icons/icon-384x384.png",
     "./icons/icon-512x512.png",
 ];
-
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -27,26 +24,25 @@ self.addEventListener('install', function (e) {
         })
     )
 });
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  });
-
-  self.addEventListener("fetch", function (e) {
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+self.addEventListener("fetch", function (e) {
     if (e.request.url.includes("/api/")) {
         e.respondWith(
             caches.open(DATA_CACHE_NAME).then(dataCache => {
